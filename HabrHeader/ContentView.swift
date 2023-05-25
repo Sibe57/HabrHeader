4002e1e6570d032b36c3f6c880eb88802b32059b
Eronin Fedor <eroninf@gmail.com>
2023-05-25 12:36:56 +0400

    Initial Commit

---
+++ HabrHeader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HabrHeader
+//
+//  Created by Eronin Fedor NP on 25.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabrHeader/HabrHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabrHeaderApp.swift
+//  HabrHeader
+//
+//  Created by Eronin Fedor NP on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabrHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
